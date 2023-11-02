5dede1e2fad779476c72d9fb33cbdccef078dfed
vikhani <vikhanieva@gmail.com>
2023-11-02 12:46:21 +0300

    Initial commit

---
+++ src/main/java/edu/vikhani/springcloudwebinardemo/SpringCloudWebinarDemoApplication.java
@@ -0,0 +1,13 @@
+package edu.vikhani.springcloudwebinardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudWebinarDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudWebinarDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/vikhani/springcloudwebinardemo/SpringCloudWebinarDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.vikhani.springcloudwebinardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudWebinarDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
