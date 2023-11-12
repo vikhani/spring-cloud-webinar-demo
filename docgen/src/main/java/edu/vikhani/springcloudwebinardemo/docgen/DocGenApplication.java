1f3396bd525b5b1a60bfab8674dffd19f9a88d48
vikhani <vikhanieva@gmail.com>
2023-11-12 23:21:19 +0300

    Add OpenFeign

--- docgen/src/main/java/edu/vikhani/springcloudwebinardemo/docgen/DocGenApplication.java
+++ docgen/src/main/java/edu/vikhani/springcloudwebinardemo/docgen/DocGenApplication.java
@@ -1,11 +1,16 @@
 package edu.vikhani.springcloudwebinardemo.docgen;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class DocGenApplication {
 
     public static void main(String[] args) {
